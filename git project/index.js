c9f0f5061be7913834bd50243ce7ef55305411aa
Rakshtih.y.n <127830167+Rakshithyn@users.noreply.github.com>
2025-06-23 16:32:00 +0530

    23

--- git project/index.js
+++ git project/index.js
@@ -1,19 +1,44 @@
 const jsonfile = require('jsonfile');
 const moment = require('moment');
-const simplegit  = require
+const simplegit  = require("simple-git")
+const random = require("random")
 
 const FILE_PATH = './date.josn';
 
+ const makeCommit = n => { 
 
-const DATE = moment().subtract(1, 'd').format();
+    if(n === 0)simplegit().push();
+    const x = random.int(0,54);
+    const y = random.int(0,6);
+    const DATE = moment().subtract(1, "y").add(1,"d").add(X, "w").add(y,"d").format();
 
 const data = {
     date: DATE
 
+     
+
 }
+   console.log(DATE);
 
-jsonfile.writeFile(FILE_PATH, data );   
+             jsonfile.writeFile(FILE_PATH, data );
 
-//git commit --date"""
+             simplegit().add([FILE_PATH]).commit (DATE, {'--date': DATE }, makeCommit.bind(this,--n) ); 
+ };
 
-simplegit().add([FILE_PATH]).commit (DATE, {'--date': DATE  }).push(); 
+makeCommit(100);
+
+//const DATE = moment().subtract(1, 'd').format();
+//const DATE = moment().date();
+// const DATE = moment().format();
+// const data = {
+//     date: DATE
+
+// }
+
+// jsonfile.writeFile(FILE_PATH, data );   
+
+// //git commit --date"""
+
+//  simplegit().add([FILE_PATH]).commit (DATE, {'--date': DATE  }).push(); 
+
+ makeCommit(100);
